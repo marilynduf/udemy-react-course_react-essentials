0d3b9883e6625ec9c1186930496ef5d4cd8cf3bb
M <marilynduf@gmail.com>
2025-08-05 19:34:02 -0400

    Création valeur dynamique

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
-import Header from "./Header.jsx";
 import "./index.css";
 
 const entryPoint = document.getElementById("root");
