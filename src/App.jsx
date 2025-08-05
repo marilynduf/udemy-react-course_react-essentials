9a626346cb2ec82e91415cc53dd5e8bf126e0394
M <marilynduf@gmail.com>
2025-08-05 19:32:51 -0400

    Retrait import composant Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
     return (
         <header>
             <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
             <h1>React Essentials</h1>
             <p>
-                Fundamental React concepts you will need for almost any app you
-                are going to build!
+                {reactDescription[getRandomInt(2)]} React concepts you will need
+                for almost any app you are going to build!
             </p>
         </header>
     );
 }
+
 function App() {
     return (
         <div>
