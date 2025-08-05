8d6884705e4c4e5d0f91cec5a2dfd798c8963aab
M <marilynduf@gmail.com>
2025-08-05 19:39:07 -0400

    Deplacement expression js dans variable

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ function getRandomInt(max) {
 }
 
 function Header() {
+    const description = reactDescription[getRandomInt(2)];
     return (
         <header>
             <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
             <h1>React Essentials</h1>
             <p>
-                {reactDescription[getRandomInt(2)]} React concepts you will need
-                for almost any app you are going to build!
+                {description} React concepts you will need for almost any app
+                you are going to build!
             </p>
         </header>
     );
