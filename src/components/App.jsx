bffc1609755eaa706edb5845b8bb92900a99f4aa
M <marilynduf@gmail.com>
2025-08-11 10:56:53 -0400

    Mapping CoreConcepts composants

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,10 +18,9 @@ function App() {
             <Header />
             <section id="core-concepts">
                 <ul>
-                    <CoreConcept {...CORE_CONCEPTS[0]} />
-                    <CoreConcept {...CORE_CONCEPTS[1]} />
-                    <CoreConcept {...CORE_CONCEPTS[2]} />
-                    <CoreConcept {...CORE_CONCEPTS[3]} />
+                    {CORE_CONCEPTS.map((conceptItems) => (
+                        <CoreConcept {...conceptItems} />
+                    ))}
                 </ul>
             </section>
             <main>
